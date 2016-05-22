b008fa12dd650f55464e5a822c1c78b31f710364
John Ferguson <aninvisiblefriend.tech@gmail.com>
2016-05-21 23:50:53 -0400

    Part 1 of refactoring - adding use of implicit class to compile users data.

--- src/main/scala/DataSciencester.scala
+++ src/main/scala/DataSciencester.scala
@@ -1,23 +1,39 @@
 package org.invisibletech.datasciencefromscratch
 
 object DataSciencester {
+  // Test data from the book.
+  // Trying to do this with immutable collections.  ^_^`
+  // A property of this simple case is that we order the  maps by id which match the indexes.
+  val users = List(
+    Map("id" -> 0, "name" -> "Hero"),
+    Map("id" -> 1, "name" -> "Dunn"),
+    Map("id" -> 2, "name" -> "Sue"),
+    Map("id" -> 3, "name" -> "Chi"),
+    Map("id" -> 4, "name" -> "Thor"),
+    Map("id" -> 5, "name" -> "Clive"),
+    Map("id" -> 6, "name" -> "Hicks"),
+    Map("id" -> 7, "name" -> "Devin"),
+    Map("id" -> 8, "name" -> "Kate"),
+    Map("id" -> 9, "name" -> "Klein"))
 
-    // Trying to do this with immutable collections.  ^_^`
-    // A property of this simple case is that we order the  maps by id which match the indexes.
-    val users = List(
-        Map("id" -> 0, "name" -> "Hero"),
-        Map("id" -> 1, "name" -> "Dunn"),
-        Map("id" -> 2, "name" -> "Sue"),
-        Map("id" -> 3, "name" -> "Chi"),
-        Map("id" -> 4, "name" -> "Thor"),
-        Map("id" -> 5, "name" -> "Clive"),
-        Map("id" -> 6, "name" -> "Hicks"),
-        Map("id" -> 7, "name" -> "Devin"),
-        Map("id" -> 8, "name" -> "Kate"),
-        Map("id" -> 9, "name" -> "Klein")
-    )
+  val friendships = List((0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9))
 
-    /*
+  val interests = List((0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data"))
+
+  /*
         NOTE:
         A lot of the code here is an experiment in using immutable collections to parallel code written 
         in Python from the book: Data Science from Scratch
@@ -32,69 +48,87 @@ object DataSciencester {
         by implementing it.  So I am trying to have fun.
     */
 
-    val friendships = List((0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9))
+  type User = Map[String, Any]
+  type UsersRaw = List[User]
+  type Friendships = List[(Int, Int)]
 
+  type DeferedFriend = scala.Function1[List[User], User]
 
-    type deferedFriend = scala.Function1[List[Map[String, Any]], Map[String, Any]]
-
-    def createBiDirectionalRelations(relations: List[(Int, Int)]): List[(Int, Int)] = {
-        relations.flatMap(r => List(r, (r._2, r._1)))
+  object CompiledUsers {
+    def createBiDirectionalRelations(relations: Friendships): Friendships = {
+      relations.flatMap(r => List(r, (r._2, r._1)))
     }
 
-    def applyFriends(users: List[Map[String, Any]], friendships: List[(Int, Int)]) = {
-        val biRelations = createBiDirectionalRelations(friendships)
+    def applyFriends(users: UsersRaw, friendships: Friendships) = {
+      val biRelations = createBiDirectionalRelations(friendships)
 
-        biRelations.groupBy(_._1).toList.map(g => users(g._1) + 
-          ("friends" -> g._2.map(r => (u: List[Map[String, Any]]) => (u.filter(m => resolveField(m, "id") == r._2).head) )))
-          .sortBy(m => m("id").asInstanceOf[Int])
+      biRelations.groupBy(_._1).toList.map(g => users(g._1) +
+        ("friends" -> g._2.map(r => (u: List[Map[String, Any]]) => (u.filter(m => resolveField(m, "id") == r._2).head))))
+        .sortBy(m => m("id").asInstanceOf[Int])
     }
+  }
 
-    def resolveListOfFriendFunctions(connectedUser: Map[String, Any]) = {
-        connectedUser.get("friends").map(_.asInstanceOf[List[deferedFriend]])
-                        .getOrElse(List[deferedFriend]())
+  implicit class CompiledUsers(rawData: (UsersRaw, Friendships)) {
+    lazy val usersWithFriends = CompiledUsers.applyFriends(rawData._1, rawData._2)
+    lazy val usersById: Map[Int, Map[String, Any]] = usersWithFriends
+                      .map(u => (u("id").asInstanceOf[Int], u))
+                      .toMap
+
+    def userById(id: Int) = {
+      usersById(id)
     }
+  }
 
-    def reifyFriends(connectedUser: Map[String, Any], users: List[Map[String, Any]]) = {
-      resolveListOfFriendFunctions(connectedUser).map(f => f(users))
-    }
+  def resolveListOfFriendFunctions(connectedUser: User) = {
+    connectedUser.get("friends").map(_.asInstanceOf[List[DeferedFriend]])
+      .getOrElse(List[DeferedFriend]())
+  }
 
-    def resolveFriendById(users: List[Map[String, Any]], selfId: Int, friendId: Int) = {
-       reifyFriends(users.filter(m => resolveField(m, "id") == selfId).head, users)
-            .filter(m => resolveField(m, "id") == friendId).head
-    }
+  def reifyFriends(connectedUser: User, users: CompiledUsers) = {
+    resolveListOfFriendFunctions(connectedUser).map(f => f(users.usersWithFriends))
+  }
 
-    def totalConnections(connectedUsers: List[Map[String, Any]]) = {
-        connectedUsers.map(numberOfFriends(_)).sum
-    }
+  def resolveFriendById(users: CompiledUsers, selfId: Int, friendId: Int) = {
+    reifyFriends(users.usersWithFriends.filter(m => resolveField(m, "id") == selfId).head, users)
+      .filter(m => resolveField(m, "id") == friendId).head
+  }
 
-    def numberOfFriends(connectedUser: Map[String, Any]) = {
-        resolveListOfFriendFunctions(connectedUser).size
-    }
+  def totalConnections(users: CompiledUsers) = {
+    users.usersWithFriends.map(numberOfFriends(_)).sum
+  }
 
-    def averageNumberOfFriends(connectedUsers: List[Map[String, Any]]) = {
-        totalConnections(connectedUsers) / connectedUsers.size.toDouble
-    }
+  def numberOfFriends(connectedUser: User) = {
+    resolveListOfFriendFunctions(connectedUser).size
+  }
 
-    def numberOfFriendsById(connectedUsers: List[Map[String, Any]]) = {
-        connectedUsers.map(u => (u("id").asInstanceOf[Int], numberOfFriends(u)))
-    }
+  def averageNumberOfFriends(users: CompiledUsers) = {
+    totalConnections(users) / users.usersWithFriends.size.toDouble
+  }
 
-    def sortedNumberOfFriendsById(numberOfFriendsById: List[(Int, Int)]) = {
-        numberOfFriendsById.sorted(Ordering.by((_: Tuple2[Int, Int])._2).reverse)    
-    }
+  def numberOfFriendsById(users: CompiledUsers) = {
+    users.usersWithFriends.map(u => (u("id").asInstanceOf[Int], numberOfFriends(u)))
+  }
 
-    def resolveField[T](connectedUser: Map[String, Any], name: String) = {
-        connectedUser.get(name).map(_.asInstanceOf[T]).getOrElse(null)
-    }
+  def sortedNumberOfFriendsById(numberOfFriendsById: Friendships) = {
+    numberOfFriendsById.sorted(Ordering.by((_: Tuple2[Int, Int])._2).reverse)
+  }
 
-    def countOfCommonFoFs(connectedUser: Map[String, Any], users: List[Map[String, Any]]) = {
-      val friendsOfUser = reifyFriends(connectedUser, users)
-      val listOfFriendIds = friendsOfUser.map(f => resolveField[Int](f, "id"))
+  def resolveField[T](connectedUser: User, name: String) = {
+    connectedUser.get(name).map(_.asInstanceOf[T]).getOrElse(null)
+  }
 
-      friendsOfUser.map(f => ((resolveField[Int](f, "id")),
-                              reifyFriends(f, users)
-                                .map(f => resolveField[Int](f, "id"))
-                                .filter(c => listOfFriendIds.contains(c))
-                                .size ))
-    }
+  def countOfCommonFoFs(connectedUser: User, users: CompiledUsers) = {
+    val friendsOfUser = reifyFriends(connectedUser, users)
+    val listOfFriendIds = friendsOfUser.map(f => resolveField[Int](f, "id"))
+
+    friendsOfUser.map(f => ((resolveField[Int](f, "id")),
+      reifyFriends(f, users)
+      .map(f => resolveField[Int](f, "id"))
+      .filter(c => listOfFriendIds.contains(c))
+      .size))
+  }
+
+  def compileInterestsToUsers(insterests: List[(Int, String)]) = {
+    interests.map(i => (i._2, i._1)).groupBy(_._1).map({ case (k, v) => (k, v.map(_._2)) })
+  }
 }
--- src/test/scala/DataSciencesterSpec.scala
+++ src/test/scala/DataSciencesterSpec.scala
@@ -5,66 +5,37 @@ import DataSciencester._
 import org.scalatest.FunSpec
 
 class DataSciencesterSpec extends FunSpec {
-  describe("Relations can be made bidirectional") {
-    it("should show graph edge both ways"){
-      val result = createBiDirectionalRelations(List((1, 2)))  
-
-      assert(result == List((1,2), (2, 1)))
-    }
-    it("will produce duplicates if 1 way relationships repeat relations") {
-      val result = createBiDirectionalRelations(List((1, 2), (2, 1)))  
-      
-      assert(result == List((1,2), (2, 1), (2, 1), (1, 2)))
-    }
-  }
-
-  describe("We should be able to establish friendships immutably and with delayed evaluation") {
-    it("should allow following friends from myself and back again") {
-        val friendships = List((0, 1))
-        val users = List(
-        Map("id" -> 0, "name" -> "Hero"),
-        Map("id" -> 1, "name" -> "Dunn"))
-
-        val result = applyFriends(users, friendships)
-
-        assert(result.size == 2)
-
-        val friendOfHero = resolveFriendById(result, 0, 1)
-        assert(friendOfHero("name") == "Dunn")
-
-        val friendOfDunn = resolveFriendById(result, 1, 0)
-        assert(friendOfDunn("name") == "Hero")
-    }
-  }
 
   describe("We should be able to get the expected statistics") {
     it("total number of connections") {
-      assert(totalConnections(applyFriends(users, friendships)) == 24)
+      assert(totalConnections((users, friendships)) == 24)
     }
 
     it("number of friends of one user") {
-      assert(numberOfFriends(applyFriends(users, friendships)(1)) == 3)
+      val compiled: CompiledUsers = (users, friendships)
+
+      assert(numberOfFriends(compiled.usersWithFriends(1)) == 3)
     }
 
     it("avg number of friends") {
-      assert(averageNumberOfFriends(applyFriends(users, friendships)) == 2.4)
+      assert(averageNumberOfFriends((users, friendships)) == 2.4)
     }
 
     it("number of friends by id") {
-      val friends = applyFriends(users, friendships)
+      val friends = (users, friendships)
       assert(numberOfFriendsById(friends).size == 10)
       assert(numberOfFriendsById(friends) == List((0,2), (1,3), (2,3), (3,3), (4,2), (5,3), (6,2), (7,2), (8,3), (9,1)))
     }
 
     it("sorted number of friends by id") {
-      val friends = numberOfFriendsById(applyFriends(users, friendships))
+      val friends = numberOfFriendsById((users, friendships))
       assert(sortedNumberOfFriendsById(friends).size == 10)
       assert(sortedNumberOfFriendsById(friends) == List((1,3), (2,3), (3,3), (5,3), (8,3), (0,2), (4,2), (6,2), (7,2), (9,1)))
     }
 
     it("counts number of friends have in common with friends") {
-      val friends = applyFriends(users, friendships)
-      assert(countOfCommonFoFs(friends(1), friends) == List((0,1), (2,2), (3,1)))
+      val friends = (users, friendships)
+      assert(countOfCommonFoFs(friends.usersWithFriends(1), friends) == List((0,1), (2,2), (3,1)))
     }
   }
 }
